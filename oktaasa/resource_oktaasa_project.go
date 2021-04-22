86bdd5eb45c8abd02dd38b3d79260fddb97c6d66
Jon Evans <jon.evans@pobox.com>
2021-04-22 17:18:27 +0100

    Add all of the missing project properties

--- oktaasa/resource_oktaasa_project.go
+++ oktaasa/resource_oktaasa_project.go
@@ -29,6 +29,44 @@ func resourceOKTAASAProject() *schema.Resource {
 				Optional: true,
 				Default:  63001,
 			},
+			"create_server_users": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
+			"force_shared_ssh_users": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
+			"forward_traffic": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
+			"rdp_session_recording": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
+			"require_preauthorization": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
+			"shared_admin_user_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"shared_standard_user_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"ssh_session_recording": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
 		},
 	}
 }
@@ -36,32 +74,26 @@ func resourceOKTAASAProject() *schema.Resource {
 func resourceOKTAASAProjectCreate(d *schema.ResourceData, m interface{}) error {
 	token := m.(Bearer)
 
-	//get project_name from terraform config.
-	project_name := d.Get("project_name").(string)
-	next_unix_uid := d.Get("next_unix_uid")
-	next_unix_gid := d.Get("next_unix_gid")
-
-	// create project in OKTAASA
-	project := map[string]interface{}{"name": project_name,
-		"create_server_users": true,
-		"next_unix_uid":       next_unix_uid,
-		"next_unix_gid":       next_unix_gid}
+	project, err := buildProjectFromResourceData(d)
+	if err != nil {
+		return err
+	}
 	projectB, _ := json.Marshal(project)
 
-	d.SetId(project_name)
+	d.SetId(project.Name)
 	log.Printf("[DEBUG] Project POST body: %s", projectB)
 
 	//make API call to create project
 	resp, err := SendPost(token.BearerToken, "/teams/"+teamName+"/projects", projectB)
 
 	if err != nil {
-		return fmt.Errorf("[ERROR] Error when creating project: %s. Error: %s", project_name, err)
+		return fmt.Errorf("[ERROR] Error when creating project: %s. Error: %s", project.Name, err)
 	}
 
 	status := resp.StatusCode()
 
 	if status == 201 {
-		log.Printf("[INFO] Project %s was successfully created", project_name)
+		log.Printf("[INFO] Project %s was successfully created", project.Name)
 	} else {
 		log.Printf("[ERROR] Something went wrong while creating project. Error: %s", resp)
 	}
@@ -69,13 +101,48 @@ func resourceOKTAASAProjectCreate(d *schema.ResourceData, m interface{}) error {
 	return resourceOKTAASAProjectRead(d, m)
 }
 
-type ProjectList struct {
-	Projects []Project `json:"list"`
+func buildProjectFromResourceData(d *schema.ResourceData) (*Project, error) {
+	project := &Project{
+		Name:                   d.Get("project_name").(string),
+		NextUnixUid:            d.Get("next_unix_uid").(int),
+		NextUnixGid:            d.Get("next_unix_gid").(int),
+		CreateServerUsers:      d.Get("create_server_users").(bool),
+		ForceSharedSshUsers:    d.Get("force_shared_ssh_users").(bool),
+		ForwardTraffic:         d.Get("forward_traffic").(bool),
+		RDPSessionRecording:    d.Get("rdp_session_recording").(bool),
+		RequirePreauth:         d.Get("require_preauthorization").(bool),
+		SharedAdminUserName:    d.Get("shared_admin_user_name").(string),
+		SharedStandardUserName: d.Get("shared_standard_user_name").(string),
+		SshSessionRecording:    d.Get("ssh_session_recording").(bool),
+	}
+
+	var err error
+	if project.ForceSharedSshUsers && (project.SharedStandardUserName == "" || project.SharedAdminUserName == "") {
+		err = fmt.Errorf("error creating resource: shared_standard_user_name and shared_admin_user_name must be provided if force_shared_ssh_users is true")
+	}
+
+	return project, err
 }
 
+/*
+type ProjectList struct {
+  Projects []Project `json:"list"`
+}
+*/
+
 type Project struct {
-	Name      string `json:"name"`
-	DeletedAt string `json:"deleted_at"`
+	Name                   string `json:"name"`
+	DeletedAt              string `json:"deleted_at"`
+	CreateServerUsers      bool   `json:"create_server_users"`
+	ForceSharedSshUsers    bool   `json:"force_shared_ssh_users"`
+	ForwardTraffic         bool   `json:"forward_traffic"`
+	NextUnixUid            int    `json:"next_unix_uid"`
+	NextUnixGid            int    `json:"next_unix_gid"`
+	RDPSessionRecording    bool   `json:"rdp_session_recording"`
+	RequirePreauth         bool   `json:"require_preauth_for_creds"`
+	SharedAdminUserName    string `json:"shared_admin_user_name,omitempty"`
+	SharedStandardUserName string `json:"shared_standard_user_name,omitempty"`
+	SshSessionRecording    bool   `json:"ssh_session_recording"`
 }
 
 func resourceOKTAASAProjectRead(d *schema.ResourceData, m interface{}) error {
@@ -103,6 +170,26 @@ func resourceOKTAASAProjectRead(d *schema.ResourceData, m interface{}) error {
 		return nil
 	} else if status == 200 && deleted == false {
 		log.Printf("[INFO] Project %s exists.", projectName)
+
+		var project Project
+
+		err := json.Unmarshal(resp.Body(), &project)
+		if err != nil {
+			return fmt.Errorf("Unable to unmarshal project settings")
+		}
+
+		d.SetId(project.Name)
+		d.Set("create_server_users", project.CreateServerUsers)
+		d.Set("force_shared_ssh_users", project.ForceSharedSshUsers)
+		d.Set("forward_traffic", project.ForwardTraffic)
+		d.Set("next_unix_uid", project.NextUnixUid)
+		d.Set("next_unix_gid", project.NextUnixGid)
+		d.Set("rdp_session_recording", project.RDPSessionRecording)
+		d.Set("require_preauthorization", project.RequirePreauth)
+		d.Set("shared_admin_user_name", project.SharedAdminUserName)
+		d.Set("shared_standard_user_name", project.SharedStandardUserName)
+		d.Set("ssh_session_recording", project.SshSessionRecording)
+
 		return nil
 	} else if status == 404 {
 		log.Printf("[INFO] Project %s does not exist", projectName)
@@ -116,34 +203,29 @@ func resourceOKTAASAProjectRead(d *schema.ResourceData, m interface{}) error {
 func resourceOKTAASAProjectUpdate(d *schema.ResourceData, m interface{}) error {
 	token := m.(Bearer)
 
-	//get project_name from terraform config.
-	projectName := d.Get("project_name").(string)
-	nextUnixUid := d.Get("next_unix_uid")
-	nextUnixGid := d.Get("next_unix_gid")
+	project, err := buildProjectFromResourceData(d)
+	if err != nil {
+		return err
+	}
 
-	// create project in OKTAASA
-	project := map[string]interface{}{"name": projectName,
-		"create_server_users": true,
-		"next_unix_uid":       nextUnixUid,
-		"next_unix_gid":       nextUnixGid}
 	projectB, _ := json.Marshal(project)
 
-	d.SetId(projectName)
+	d.SetId(project.Name)
 	log.Printf("[DEBUG] Project POST body: %s", projectB)
 
 	//make API call to create project
-	resp, err := SendPut(token.BearerToken, "/teams/"+teamName+"/projects/"+projectName, projectB)
+	resp, err := SendPut(token.BearerToken, "/teams/"+teamName+"/projects/"+project.Name, projectB)
 
 	if err != nil {
-		return fmt.Errorf("[ERROR] Error updating project settings. Project: %s. Error: %s", projectName, err)
+		return fmt.Errorf("[ERROR] Error updating project settings. Project: %s. Error: %s", project.Name, err)
 	}
 
 	status := resp.StatusCode()
 
 	if status == 204 {
-		log.Printf("[INFO] Project %s was successfully updated", projectName)
+		log.Printf("[INFO] Project %s was successfully updated", project.Name)
 	} else {
-		return fmt.Errorf("[ERROR] Something went wrong while updating the project %s. Error: %s", projectName, resp)
+		return fmt.Errorf("[ERROR] Something went wrong while updating the project %s. Error: %s", project.Name, resp)
 
 	}
 
